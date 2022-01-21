e1a218d2875aa01c6958b6b39e319ce9a24b84a5
thejanaakmeemana <thejanaakmeemana@gmail.com>
2022-01-21 22:33:20 +0530

    first commit

--- app/Views/view_data.php
+++ app/Views/view_data.php
@@ -8,8 +8,8 @@
     <ul>
     <?php 
         foreach ($result->getResult() as $row) :
-        $absolute_path = "home/thejana/Documents/assignments/is1109/ci/project-root/writable/uploads/".$row->userfile;
-    ?>                                
+        $absolute_path = "/home/thejana/Documents/assignments/is1109/ci/project-root/writable/uploads/".$row->userfile;
+    ?>                            
     <li>
         <img src="<?php echo base_url($absolute_path); ?>" width="50px" />
         <?php echo $row->image_title; ?>
